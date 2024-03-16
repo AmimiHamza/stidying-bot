87b45085563647bf2000327f0d26c3ac8672d98c
AmimiHamza <amimi1hamza@gmail.com>
2024-03-16 17:08:53 +0100

    first commit

---
+++ constents.py
@@ -0,0 +1,6 @@
+from typing import Final
+greeting=['hello','hi','hey','slm','salam']
+TOKEN: Final='6431996793:AAHq1EkJxXTueR5v_LFTEL-f2spuwcF2MIo'
+BOT_USERNAME: Final='@the_dse_bot'
+commands=['get','show','add','delete','update']
+administateurs=[6452552093,5467008406]
